776dfe42da51389fb15a5b74bf02bbd311fa60cb
Dotsially <mateu9999@gmail.com>
2025-02-22 17:36:19 +0100

    Fixed bat file

--- build_run.bat
+++ build_run.bat
@@ -1,5 +1,4 @@
 @echo off
 set PATH=%PATH%;C:\w64devkit\bin
 g++ -O1 src/*.cpp src_dependecies/*.c -o bin/world.exe -Wall -Wno-missing-braces -I include/. -I header/. -L lib -lSDL2 -lSDL2main -lopengl32 -lgdi32 -llua54 -lenet64 -lwinmm -lws2_32 -luser32 -lshell32
-bin\world.exeg++ -O1 src/*.cpp src_dependecies/*.c -o bin/world.exe -Wall -Wno-missing-braces -I include/. -I header/. -L lib -lSDL2 -lSDL2main -lopengl32 -lgdi32 -llua54 -lenet64 -lwinmm -lws2_32 -luser32 -lshell32
 bin\world.exe
